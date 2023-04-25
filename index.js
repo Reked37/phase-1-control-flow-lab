167cf159595c51e996e55066c00e738acecd7bd3
Derek Truesdale <dtruesdale37@gmail.com>
2023-04-25 14:07:13 -0500

    lab completed

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet<400)
+    return 'This one is on me!'
+ else if(feet>400 && feet<2000)
+   return 'That will be twenty bucks.'
+ else if (feet>2000 && feet<2499)
+    return 'I will gladly take your thirty bucks.'
+ else (feet>=2500)
+    return 'No can do.'
+}
+function ternaryCheckCity(city){
+  const checkCity = (city === 'NYC')? "Ok, sounds good.": "No go."
+  return checkCity
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+let charm;
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      charm = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      charm = 'Thank you.'
+      break;
+    case 'thanks for everything':
+      charm = 'Bye.'
+      break;
+  }
+  return charm
 }
